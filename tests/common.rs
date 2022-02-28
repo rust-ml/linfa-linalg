f5ee4459e220dd18f39b997b2950dd115257be3d
YuhanLiin <yuhanliin+github@protonmail.com>
2022-02-27 23:46:43 -0500

    Add tridiagonal proptests

--- tests/common.rs
+++ tests/common.rs
@@ -1,16 +1,33 @@
+#![allow(unused)]
+
 use std::ops::RangeInclusive;
 
 use ndarray::prelude::*;
 use proptest::prelude::*;
 
-#[allow(unused)]
 const FLOAT_RANGE: RangeInclusive<f64> = -1000.0..=1000.0;
-#[allow(unused)]
 const DIM_RANGE: RangeInclusive<usize> = 1..=10;
 
 prop_compose! {
     pub fn square_arr()(dim in DIM_RANGE)
         (data in prop::collection::vec(FLOAT_RANGE, dim*dim), dim in Just(dim)) -> Array2<f64> {
         Array2::from_shape_vec((dim, dim), data).unwrap()
+    }
+}
+
+// TODO offer this in the main crate API
+fn to_symm(arr: &mut Array2<f64>) {
+    let n = arr.nrows();
+    for i in 0..n {
+        for j in 0..i {
+            arr[(i, j)] = arr[(j, i)];
+        }
+    }
+}
+
+prop_compose! {
+    pub fn symm_arr()(mut arr in square_arr()) -> Array2<f64> {
+        to_symm(&mut arr);
+        arr
     }
 }
---
+++ tests/tridiagonal.rs
@@ -0,0 +1,42 @@
+use approx::assert_abs_diff_eq;
+use ndarray::Array2;
+use num_traits::Zero;
+use proptest::prelude::*;
+
+use ndarray_linalg_rs::tridiagonal::*;
+
+mod common;
+
+// Assume arr is symmetric
+fn run_tridiagonal_test(arr: &Array2<f64>) {
+    let n = arr.nrows();
+    let decomp = arr.clone().sym_tridiagonal().unwrap();
+    let q = decomp.generate_q();
+    let tri = decomp.into_tridiag_matrix();
+    // Ensure it's actually tridiagonal
+    for ((i, j), e) in tri.indexed_iter() {
+        if (i as i64 - j as i64).abs() > 1 {
+            assert!(e.is_zero());
+        }
+    }
+
+    // Q * T * Q.t must equal arr
+    assert_abs_diff_eq!(q.dot(&tri).dot(&q.t()), arr, epsilon = 1e-7);
+    // Q must be orthogonal
+    assert_abs_diff_eq!(q.dot(&q.t()), Array2::eye(n), epsilon = 1e-7);
+}
+
+proptest! {
+    #[test]
+    fn tridiagonal_test(arr in common::symm_arr()) {
+        run_tridiagonal_test(&arr);
+    }
+
+    #[test]
+    // Just make sure it doesn't crash, we know the answer will be wrong
+    fn tridiagonal_non_symm(arr in common::square_arr()) {
+        let decomp = arr.sym_tridiagonal().unwrap();
+        decomp.generate_q();
+        decomp.into_tridiag_matrix();
+    }
+}
