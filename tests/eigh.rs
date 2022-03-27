80c23fc87c2d72e444e267d7d825cb00ba0cd177
YuhanLiin <yuhanliin+github@protonmail.com>
2022-03-27 01:40:40 -0400

    Add sorting to eigh tests

--- tests/eigh.rs
+++ tests/eigh.rs
@@ -6,25 +6,40 @@ use ndarray_linalg_rs::eigh::*;
 
 mod common;
 
-fn run_eigh_test(arr: Array2<f64>) {
-    let n = arr.nrows();
-    let (vals, vecs) = arr.eigh().unwrap();
-    assert_abs_diff_eq!(arr.eigvalsh().unwrap(), vals, epsilon = 1e-5);
-    // Eigenvecs should be orthogonal
-    let s = vecs.t().dot(&vecs);
-    assert_abs_diff_eq!(s, Array2::eye(n), epsilon = 1e-5);
+fn check_eigh(arr: &Array2<f64>, vals: &Array1<f64>, vecs: &Array2<f64>) {
     // Original array multiplied with eigenvec should equal eigenval times eigenvec
     for (i, v) in vecs.axis_iter(Axis(1)).enumerate() {
         let av = arr.dot(&v);
         let ev = v.mapv(|x| vals[i] * x);
         assert_abs_diff_eq!(av, ev, epsilon = 1e-5);
     }
+}
+
+fn run_eigh_test(arr: Array2<f64>) {
+    let n = arr.nrows();
+    let d = arr.eigh().unwrap();
+    let (vals, vecs) = d.clone();
+    assert_abs_diff_eq!(arr.eigvalsh().unwrap(), vals, epsilon = 1e-5);
+    // Eigenvecs should be orthogonal
+    let s = vecs.t().dot(&vecs);
+    assert_abs_diff_eq!(s, Array2::eye(n), epsilon = 1e-5);
+    check_eigh(&arr, &vals, &vecs);
 
     let (evals, evecs) = arr.clone().eigh_into().unwrap();
     assert_abs_diff_eq!(evals, vals);
     assert_abs_diff_eq!(evecs, vecs);
-    let evals = arr.eigvalsh_into().unwrap();
+    let evals = arr.clone().eigvalsh_into().unwrap();
     assert_abs_diff_eq!(evals, vals);
+
+    // Check if ascending eigen is actually sorted and valid
+    let (vals, vecs) = d.clone().sort_eig_asc();
+    check_eigh(&arr, &vals, &vecs);
+    assert!(vals.windows(2).into_iter().all(|w| w[0] <= w[1]));
+
+    // Check if descending eigen is actually sorted and valid
+    let (vals, vecs) = d.sort_eig_desc();
+    check_eigh(&arr, &vals, &vecs);
+    assert!(vals.windows(2).into_iter().all(|w| w[0] >= w[1]));
 }
 
 proptest! {
